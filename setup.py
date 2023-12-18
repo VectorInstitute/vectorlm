c191805ced1521bef605bb41ec8546dbc4bb0897
adil-a <adil.asif2000@hotmail.com>
2023-12-18 03:50:47 -0500

    added requirements and updated setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
 
 setup(
     name="medgpt",
-    version="0.1",
+    version="1.0",
     packages=find_packages(),
+    install_requires=requirements,
+    python_requires=">=3.10",
 )
